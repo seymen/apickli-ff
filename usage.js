c1e1ac55e3e6828df8a227050f748ba044ba37ba
Ozan Seymen <oseymen@gmail.com>
2018-08-22 21:46:39 +0100

    moving apickli.js in lib

--- usage.js
+++ usage.js
@@ -1,31 +1,31 @@
-require('./apickli').expose(global)
+require('./lib/apickli').expose(global)
 
 const ctx = {
-  variables: {
-    a: 1,
-    connection: 'close1'
-  }
+    variables: {
+        a: 1,
+        connection: 'close1'
+    }
 }
 
 const req = {
-  baseUrl: 'https://httpbin.org',
-  headers: {
-    Authorization: 'Bearer abcd'
-  }
+    baseUrl: 'https://httpbin.org',
+    headers: {
+        Authorization: 'Bearer abcd'
+    }
 }
 
 const scenarioContext = ScenarioContext(ctx)
 
 const request = RequestFactory(req)
-  .step(setHeader('map', '`a`'))
-  .stepWithContext(setQueryParameter('a', '`a`'))
-  .step(setMethod('GET'))
-  .step(setUri('/status/400'))
-  .stepWithContext(inspectTemplated)
+    .step(setHeader('map', '`a`'))
+    .stepWithContext(setQueryParameter('a', '`a`'))
+    .step(setMethod('GET'))
+    .step(setUri('/status/400'))
+    .stepWithContext(inspectTemplated)
 
 console.log('before')
 request.execute(scenarioContext)
-  .then(assertResponseCode(400))
-  .then(assertResponseHeaderValue('Connection', '`connection`'))
-  .catch(err => console.error(err))
+    .then(assertResponseCode(400))
+    .then(assertResponseHeaderValue('Connection', '`connection`'))
+    .catch(err => console.error(err))
 console.log('after')
