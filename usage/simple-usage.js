fcff9b5cf6dd2e063599a077827527f37b395ac0
Ozan Seymen <oseymen@gmail.com>
2019-02-10 00:36:20 +0000

    working with Task but using chain for execute - which is not very nice

--- usage/simple-usage.js
+++ usage/simple-usage.js
@@ -7,8 +7,6 @@ const ctx = {
   }
 }
 
-//TODO: CAN ALL OF THESE BE READER?
-
 const req = {
   baseUrl: 'https://httpbin.org',
   headers: {
@@ -16,22 +14,33 @@ const req = {
   }
 }
 
-const request = apickli
-  .request(req)
-  .chain(apickli.setHeader('map', '`a`'))
-  .chain(apickli.setMethod('GET'))
-  .chain(apickli.setUri('/get?q=`a`'))
-  // .chain(apickli.inspect)
+apickli.request(req, ctx)
+.map(apickli.setHeader('Header1', 'a'))
+.map(apickli.setUri('/get'))
+.chain(apickli.execute)
+.fork(
+  console.error,
+  r => console.log('forked:', r)
+)
+// .either(console.error, console.log)
+// .map(apickli.assertResponseCode(200))
 
-console.log('standalone usage:')
+// const request = apickli
+//   .request(req)
+//   .chain(apickli.setHeader('map', '`a`'))
+//   .chain(apickli.setMethod('GET'))
+//   .chain(apickli.setUri('/get?q=`a`'))
+//   // .chain(apickli.inspect)
 
-request
-  .execute(ctx)
-  .map(apickli.assertResponseCode(200))
-  .map(apickli.assertResponseCode(200))
-  // .map(apickli.assertResponseBodyPath('$.headers.Connection', 'close'))
-  // .map(resp => resp.body)
-  // .map(JSON.parse)
-  .fork(console.error, console.log)
+// console.log('standalone usage:')
 
-console.log('after')
+// request
+//   .execute(ctx)
+//   .map(apickli.assertResponseCode(200))
+//   .map(apickli.assertResponseCode(200))
+//   // .map(apickli.assertResponseBodyPath('$.headers.Connection', 'close'))
+//   // .map(resp => resp.body)
+//   // .map(JSON.parse)
+//   .fork(console.error, console.log)
+
+// console.log('after')
